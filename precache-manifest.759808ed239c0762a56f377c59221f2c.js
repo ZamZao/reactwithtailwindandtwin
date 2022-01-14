85b2a3e03ae591ecdcd99cbe9280dc33f42a7fe2
zam <zhamny@gmail.com>
2022-01-14 09:53:10 +0300

    Updates

---
+++ precache-manifest.759808ed239c0762a56f377c59221f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5afcaa55fc83a7d2d3e2",
+    "url": "/ZamZao/reactwithtailwindandtwin/static/css/main.9450bd54.chunk.css"
+  },
+  {
+    "revision": "5afcaa55fc83a7d2d3e2",
+    "url": "/ZamZao/reactwithtailwindandtwin/static/js/main.aa915e9e.chunk.js"
+  },
+  {
+    "revision": "5285b522efbb4564aaa3",
+    "url": "/ZamZao/reactwithtailwindandtwin/static/js/runtime~main.72824710.js"
+  },
+  {
+    "revision": "a2b01e1f47ae357407ae",
+    "url": "/ZamZao/reactwithtailwindandtwin/static/js/2.d705a351.chunk.js"
+  },
+  {
+    "revision": "e3d02a112c555a031ed5f4fadc0829ff",
+    "url": "/ZamZao/reactwithtailwindandtwin/index.html"
+  }
+];
