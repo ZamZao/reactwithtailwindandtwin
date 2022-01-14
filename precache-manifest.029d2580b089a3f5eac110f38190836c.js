9fb8785f04fd7c15d6956d78d33c1b220fe8a2a9
zam <zhamny@gmail.com>
2022-01-14 10:04:18 +0300

    Updates

---
+++ precache-manifest.029d2580b089a3f5eac110f38190836c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f5547e4daae759c8aca",
+    "url": "/reactwithtailwindandtwin/static/css/main.9450bd54.chunk.css"
+  },
+  {
+    "revision": "9f5547e4daae759c8aca",
+    "url": "/reactwithtailwindandtwin/static/js/main.72be8eba.chunk.js"
+  },
+  {
+    "revision": "afffa6af79da65b10a12",
+    "url": "/reactwithtailwindandtwin/static/js/runtime~main.2e13c74e.js"
+  },
+  {
+    "revision": "24cf6a6779e88e017851",
+    "url": "/reactwithtailwindandtwin/static/js/2.7bd9d399.chunk.js"
+  },
+  {
+    "revision": "397567da55ef5f98ac234c148afca88e",
+    "url": "/reactwithtailwindandtwin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ae59936d320686eb23dfeea0ac1eb61.js"
+  "/reactwithtailwindandtwin/precache-manifest.029d2580b089a3f5eac110f38190836c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reactwithtailwindandtwin/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
